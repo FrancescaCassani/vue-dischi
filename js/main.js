11c86d35520953629a61017f366741038ba276b8
Francesca <fracassani@gmail.com>
2020-11-24 15:09:05 +0100

    Call API

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@
 const spotify = new Vue ({
   el: "#spotify",
     data: {
-
+      albums: []
   },
   created(){  
+        //Call API
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then( result => {
+          console.log(result.data);
 
+          this.albums = result.data.response; 
+        })
+        .catch( error => {
+          console.log(error);
+        });
   },
   methods: {
    
